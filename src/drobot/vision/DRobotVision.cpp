7abc69ca3ae45c6f619b9d5d3d4f231ee58862ef
Tobias Klauser <tklauser@distanz.ch>
2013-07-12 18:02:38 +0200

    DRobotVision: Implement segmentation for ball tracking

--- src/drobot/vision/DRobotVision.cpp
+++ src/drobot/vision/DRobotVision.cpp
@@ -8,49 +8,109 @@
 
 namespace drobot {
 
-DRobotVision::DRobotVision() {
-
+DRobotVision::DRobotVision()
+{
 	capture = new DRobotDC1394Device();
+	init();
+}
 
-	lp = new cv::LogPolar_Adjacent(FRAME_WIDTH, FRAME_HEIGHT,
-			cv::Point2i(FRAME_WIDTH / 2, FRAME_HEIGHT / 2)); //, 50, 3, 0.25, 1, 70, 0);
-
-	capture->start();
-
-	std::cerr << "Capturing from constructor" << std::endl;
-	usleep(200000);
+DRobotVision::DRobotVision(int idx, int fps)
+{
+	capture = new DRobotDC1394Device(idx, fps);
+	init();
 }
 
 DRobotVision::~DRobotVision() {
 
 }
 
+void DRobotVision::init()
+{
+	lp = new cv::LogPolar_Adjacent(FRAME_WIDTH, FRAME_HEIGHT,
+			cv::Point2i(FRAME_WIDTH / 2, FRAME_HEIGHT / 2)); //, 50, 3, 0.25, 1, 70, 0);
+
+	capture->start();
+
+	usleep(200000);
+}
+
 cv::Mat DRobotVision::getFrame() {
 	frame = capture->getFrame();
+
+	// provide resized and grayscale version of image for further processing
+	resize(frame, frameResized, cv::Size(FRAME_WIDTH, FRAME_HEIGHT));
+	cv::cvtColor(frameResized, frameGray, CV_BGR2GRAY);
 
 	return frame;
 }
 
-void DRobotVision::applyTransforms() {
+void DRobotVision::applySegmentation(cv::Scalar minThresh, cv::Scalar maxThresh)
+{
+	cv::cvtColor(frameResized, frameHsv, CV_BGR2HSV);
+
+	// H is allowed to wrap around
+	if (minThresh[0] > maxThresh[0]) {
+		double tmp = minThresh[0];
+		minThresh[0] = 0.0;
+		inRange(frameHsv, minThresh, maxThresh, frameFiltered);
+		minThresh[0] = tmp;
+		maxThresh[1] = 180;
+		inRange(frameHsv, minThresh, maxThresh, frameTmp);
+		bitwise_or(frameFiltered, frameTmp, frameFiltered);
+	} else {
+		inRange(frameHsv, minThresh, maxThresh, frameFiltered);
+	}
+
+	GaussianBlur(frameFiltered, frameFiltered, cv::Size(9, 9), 0.0);
+
+	std::vector<cv::Vec3f> circles;
+	HoughCircles(frameFiltered, circles, CV_HOUGH_GRADIENT, 2,
+		     frameFiltered.rows / 4, 200, 20, 3, 12);
+
+	// just take the first one
+	if (circles.size() > 0) {
+		cv::Point center(cvRound(circles[0][0]), cvRound(circles[0][1]));
+		int radius = cvRound(circles[0][2]);
+//		std::cerr << "  x/y/r: " << center << " " << radius << std::endl;
+		lastCenter = center;
+		lastRadius = radius;
+	}
+
+	// draw the circle center
+//	circle(frameResized, lastCenter, 3, cv::Scalar(0, 255, 0), -1, 8, 0);
+	// draw the circle outline
+	circle(frameResized, lastCenter, lastRadius, cv::Scalar(0, 0, 255), -1, 8, 0);
+
+	frameSegmented = cv::Mat::zeros(frameFiltered.size(), CV_8UC1);
+	circle(frameSegmented, lastCenter, lastRadius, cv::Scalar(255, 255, 255), -1, 8, 0);
+}
+
+void DRobotVision::applyTransforms()
+{
 	ppFrameLPCartesian = pFrameLPCartesian;
 	ppFrameLPCortical = pFrameLPCortical;
 	pFrameLPCartesian = frameLPCartesian;
 	pFrameLPCortical = frameLPCortical;
 
-	resize(frame, frameResized, cv::Size(FRAME_WIDTH, FRAME_HEIGHT));
-
-	cv::cvtColor(frameResized, frameGray, CV_BGR2GRAY);
-
 	frameLPCortical = lp->to_cortical(frameGray);
 	frameLPCartesian = lp->to_cartesian(frameLPCortical);
 
+	pFrameSegLPCartesian = frameSegLPCartesian;
+	pFrameSegLPCortical = frameSegLPCortical;
+
+	frameSegLPCortical = lp->to_cortical(frameSegmented);
+	frameSegLPCartesian = lp->to_cartesian(frameSegLPCortical);
+
 	resize(frameLPCartesian, frameLPCartesian,
+			cv::Size(FRAME_WIDTH * 2, FRAME_HEIGHT * 2));
+	resize(frameSegLPCartesian, frameSegLPCartesian,
 			cv::Size(FRAME_WIDTH * 2, FRAME_HEIGHT * 2));
 
 	if (pFrameLPCartesian.size().width != 0) {
 		absdiff(frameLPCortical, pFrameLPCortical, tdFrameLPCortical);
 		absdiff(frameLPCartesian, pFrameLPCartesian, tdFrameLPCartesian);
 
+		/*
 		if (ppFrameLPCartesian.size().width != 0) {
 			// 2nd order temporal difference: td2 = f(x) - 2*f(x-1) + f(x-2)
 			td2FrameLPCortical = frameLPCortical - pFrameLPCortical;
@@ -61,6 +121,12 @@ void DRobotVision::applyTransforms() {
 			td2FrameLPCartesian += ppFrameLPCartesian;
 			absdiff(td2FrameLPCartesian, pFrameLPCartesian, td2FrameLPCartesian);
 		}
+		*/
+	}
+
+	if (pFrameSegLPCartesian.size().width != 0) {
+		absdiff(frameSegLPCortical, pFrameSegLPCortical, tdFrameSegLPCortical);
+		absdiff(frameSegLPCartesian, pFrameSegLPCartesian, tdFrameSegLPCartesian);
 	}
 }
 
@@ -85,6 +151,11 @@ double DRobotVision::getTDPixelActivity() {
 
 double DRobotVision::getTD2PixelActivity() {
 	return getTDPixelActivity(td2FrameLPCortical);
+}
+
+cv::Point DRobotVision::getLastBallCenter()
+{
+	return lastCenter;
 }
 
 }
--- src/drobot/vision/DRobotVision.h
+++ src/drobot/vision/DRobotVision.h
@@ -12,27 +12,37 @@ namespace drobot {
 class DRobotVision {
 
 private:
+	void init();
+
 	double getTDPixelActivity(cv::Mat tdFrame);
 
 public:
 
 	DRobotVision();
+	DRobotVision(int idx, int fps);
 
 	virtual ~DRobotVision();
 
 	cv::Mat getFrame();
 
 	void applyTransforms();
+	void applySegmentation(cv::Scalar minThresh, cv::Scalar maxThresh);
 
 	double getTDPixelActivity();
 	double getTD2PixelActivity();
+
+	cv::Point getLastBallCenter();
 
 private:
 
 	DRobotCapture* capture;
 
 	cv::LogPolar_Adjacent* lp;
-//	CvCapture* capture2;
+
+	cv::Mat frameTmp;		// temporary storage
+
+	cv::Point lastCenter;
+	int lastRadius;
 
 public:
 
@@ -43,17 +53,27 @@ public:
 	cv::Mat frameResized;		// current frame resized
 	cv::Mat frameGray;		// current frame in grey scale
 
+	cv::Mat frameHsv;		// current frame in HSV color space
+	cv::Mat frameFiltered;		// current frame filtered
+	cv::Mat frameSegmented;		// current frame segmented
+
 	cv::Mat frameLPCortical;	// current log-polar frame in cortical coordinates
 	cv::Mat pFrameLPCortical;	// previous log-polar frame
 	cv::Mat ppFrameLPCortical;	// pre-previous log-polar frame
 	cv::Mat tdFrameLPCortical;	// temporal-difference log-polar frame in cartesian coordinates
 	cv::Mat td2FrameLPCortical;	// 2nd order temporal-difference log-polar frame in cartesian coordinates
+	cv::Mat frameSegLPCortical;
+	cv::Mat pFrameSegLPCortical;
+	cv::Mat tdFrameSegLPCortical;
 
 	cv::Mat frameLPCartesian;	// current log-polar frame in cartesian coordinates
 	cv::Mat pFrameLPCartesian;	// previous log-polar frame
 	cv::Mat ppFrameLPCartesian;	// pre-previous log-polar frame
 	cv::Mat tdFrameLPCartesian;	// temporal-difference log-polar frame in cartesian coordinates
+	cv::Mat frameSegLPCartesian;
 	cv::Mat td2FrameLPCartesian;	// 2nd order temporal-difference log-polar frame in cartesian coordinates
+	cv::Mat pFrameSegLPCartesian;
+	cv::Mat tdFrameSegLPCartesian;
 };
 
 }
